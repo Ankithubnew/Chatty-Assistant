927fe549faff986201e20c6770754038f6def26d
Ankithubnew <rockaniket66@gmail.com>
2023-05-22 11:01:44 +0530

    addk

--- index.js
+++ index.js
@@ -10,9 +10,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/webhook", async (req, res) => {
-    res.send("1875738765");
-});
+app.get('/webhook', (req, res) => {
+    const { query } = req;
+    const { hubMode, hubVerifyToken, hubChallenge } = query;
+  
+    if (hubMode === 'subscribe' && hubVerifyToken === 'EAAbKM7FvGKUBAAEMhLZCF6pNekCzyAD2CUPZCCBL75') {
+      res.status(200).send(hubChallenge);
+    } else {
+      res.sendStatus(403); 
+    }
+  });
+  
 app.get("/",(req,res)=>{
     res.send("welcome");
 })
